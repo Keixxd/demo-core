5cf01393b029e31efee5b65713ae650db452f963
Mark Sharapov <marksharapov.workspace@gmail.com>
2022-11-28 20:14:51 +0300

    init commit

---
+++ app/src/main/java/com/example/demo/App.kt
@@ -0,0 +1,9 @@
+package com.example.demo
+
+import com.example.demo_core.app.CoreApplication
+
+class App : CoreApplication() {
+
+
+
+}
---
+++ demo_core/src/main/java/com/example/demo_core/app/CoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo_core.app
+
+import android.app.Application
+
+open class CoreApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
